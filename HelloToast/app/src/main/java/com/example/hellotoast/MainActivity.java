b75780d904d004b4b9e79ac93f3898fe984befde
scharffc <scharffc@gmail.com>
2024-02-17 11:55:46 -0500

    HelloToast added

---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+        final Intent myIntent = new Intent(Intent.ACTION_SENDTO);
+        myIntent.setData(Uri.parse("smsto:" + Uri.encode("12123461016")));
+        myIntent.putExtra("sms_body", "hello");
+        startActivity(myIntent);
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
